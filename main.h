1faf6da184127d4d8ff5a8f0368c19be4dd684be
Donesrom <donesrom@gmail.com>
2022-04-25 01:41:37 -0700

    new changes to main.h

--- main.h
+++ main.h
@@ -5,8 +5,8 @@
 #include <stdarg.h>
 
 /**
- *  struct flags - struct containing flags to "turn on"
- *  when a flag specifier is passed to _printf()
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
  * @plus: flag for the '+' character
  * @space: flag for the ' ' character
  * @hash: flag for the '#' character
@@ -19,6 +19,12 @@ int space;
 int hash;
 } flags_t;
 
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
 typedef struct printHandler
 {
 char c;
